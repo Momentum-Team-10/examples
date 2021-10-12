ea751ed7f31688daa4d145ea12acaadff8b98f3c
jeanetteobr <jeanette.obr@gmail.com>
2021-10-12 15:48:57 -0400

    add js object example

--- js-example/main.js
+++ js-example/main.js
@@ -1,6 +1,7 @@
 let people = ['Jeanette', 'Zack', 'Janelle', 'Trey', 'James', 'Lila', 'Jason', 'Jonathan']
 
 function remove(array, arrayItem) {
+    debugger
     if (array.indexOf(arrayItem === false)) {
         return array
     } else {
